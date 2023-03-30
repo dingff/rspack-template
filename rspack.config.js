9f03fe57137427742af9ef73b7370ce40ad0fa65
dingff <dingff0923@foxmail.com>
2023-03-30 15:09:23 +0800

    feat: init

---
+++ rspack.config.js
@@ -0,0 +1,81 @@
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MpaRspackPlugin = require('mpa-rspack-plugin')
+
+const isProduction = process.env.NODE_ENV === 'production'
+const devPublicPath = '/'
+const port = '8888'
+
+module.exports = {
+  entry: {},
+  output: {
+    filename: isProduction ? '[name].[contenthash:6].js' : '[name].js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: './',
+  },
+  builtins: {
+    html: [],
+    progress: true,
+    css: {
+      modules: {
+        localIdentName: '[local]--[hash:6]',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  devServer: {
+    port,
+    devMiddleware: {
+      publicPath: devPublicPath,
+    },
+  },
+  plugins: [new CleanWebpackPlugin(), new MpaRspackPlugin({
+    html: {
+      template: './index.html',
+    },
+    mountElementId: 'root',
+    lowerCase: true,
+    layout: './src/layouts/index.tsx',
+    open: `http://localhost:${port}${devPublicPath}`,
+  })],
+  mode: isProduction ? 'production' : 'development',
+  context: __dirname,
+  devtool: isProduction ? false : 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: 'sass-loader',
+          },
+        ],
+        type: 'css/module',
+      },
+      {
+        test: /^(?!.*\.module).*\.s[ac]ss$/,
+        use: [{
+          loader: 'sass-loader',
+        }],
+        type: 'css',
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|bmp|tiff|svg|woff|ttf)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+}
+
